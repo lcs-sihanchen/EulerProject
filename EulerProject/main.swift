d1f18a15111ab7507a5ee919c33b72dc758ec4e8
Chen Sihan <schen@lcs.on.ca>
2020-01-07 11:28:50 -0500

    Initial Commit

---
+++ EulerProject/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  EulerProject
+//
+//  Created by Chen, Sihan on 2020-01-07.
+//  Copyright © 2020 Chen, Sihan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
